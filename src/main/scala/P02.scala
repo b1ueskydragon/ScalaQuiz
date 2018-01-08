79543d65eaed2cf138d6e8e77813a02bcb3be4c9
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2018-01-08 15:06:39 +0900

    q2

--- src/main/scala/P02.scala
+++ src/main/scala/P02.scala
@@ -1,10 +1,16 @@
 object P02 {
   def main(args: Array[String]) {
-    print(penultimate(Dummy.myList))
+    println(penultimate(Dummy.myList))
+    print(initPra(Dummy.myList))
   }
 
   def penultimate[A](l: List[A]): A = {
     if (l.isEmpty) throw new NoSuchElementException
     l.init.last
+  }
+
+  def initPra(l: List[Int])= {
+    if (l.isEmpty) throw new NoSuchElementException
+    l.init
   }
 }
