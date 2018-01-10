9cb1d6a4393925cc8b8d230f82713ebde997ea0e
jang <jang_inhwa@microad.co.jp>
2018-01-10 14:17:57 +0900

    q1 add recursion case

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -1,10 +1,14 @@
 object P01 {
   def main(args: Array[String]): Unit = {
-    print(last(Dummy.myList))
+    println(last(Dummy.myList))
+    println(lastRecursion(Dummy.myList))
   }
 
-  def last[A](l: List[A]): A = {
-  if(l.isEmpty) throw new NoSuchElementException
-    l.last
+  def last[A](l: List[A]): A = l.last // last() already throws the NoSuchElementException
+
+  def lastRecursion[A](l: List[A]): A = l match {
+    case h :: Nil => h // one head element and a tail formed by nothing
+    case _ :: tail => lastRecursion(tail) // takes the tail of the list and calls itself on the remaining values (the first element is not even stored, it is ignored using the _ wildcard)
+    case _ => throw new NoSuchElementException // when the list is empty
   }
 }
