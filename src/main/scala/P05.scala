9ef9af18b1b17fbd326695bfe866f884b0f4530f
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2018-01-17 07:59:08 +0900

    Additional Recursion

--- src/main/scala/P05.scala
+++ src/main/scala/P05.scala
@@ -2,6 +2,8 @@ object P05 {
   def main(args: Array[String]): Unit = {
     println(reverse(Dummy.myList))
     println(reverseRecursion(Dummy.myList))
+
+    print(reverseHelper(List(1, 2, 3, 4, 5)))
   }
 
   def reverse[A](l: List[A]): List[A] = l.reverse
@@ -10,6 +12,16 @@ object P05 {
     case h :: tail => reverseRecursion(tail) ::: List(h)
     case Nil => Nil
   }
+
   // :: 要素を List に連結
   // ::: List 連結
+
+  def reverseHelper[A](l: List[A]): List[A] = {
+    def _reverse(res: List[A], rem: List[A]): List[A] = rem match {
+      case Nil => res
+      case h :: tail => _reverse(h :: res, tail)
+    }
+
+    _reverse(Nil, l)
+  }
 }
