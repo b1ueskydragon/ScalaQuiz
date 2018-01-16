360ab1d6a1a7428fdfd6fa303ead5a06243a231c
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2018-01-17 07:59:18 +0900

    q6

--- src/main/scala/P06.scala
+++ src/main/scala/P06.scala
@@ -1,3 +1,14 @@
 object P06 {
+  def main(args: Array[String]): Unit = {
+    println(isPalindrome(List(1, 2, 3, 2, 1)))
+    println(isPalindromeRecursion(List(1, 2, 3, 2, 1)))
+  }
 
+  def isPalindrome[A](l: List[A]): Boolean = l.reverse == l
+
+  def isPalindromeRecursion[A](l: List[A]): Boolean = l match {
+    case Nil => true
+    case List(a) => true
+    case list => list.head == list.last && isPalindromeRecursion(list.tail.init)
+  }
 }
