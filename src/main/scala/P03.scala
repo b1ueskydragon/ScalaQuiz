e9d28f6a315cbcaf665a06a8036b130c662a9df9
jang <jang_inhwa@microad.co.jp>
2018-01-23 17:41:12 +0900

    Add recursive method

--- src/main/scala/P03.scala
+++ src/main/scala/P03.scala
@@ -1,12 +1,20 @@
 object P03 {
   def main(args: Array[String]): Unit = {
-    print(nth(9, Dummy.myList))
+    println(findKth(9, Dummy.yourList))
+    println(findKthRecursion(9, Dummy.yourList))
   }
 
-  def nth[A](k: Int, l: List[A]): A = {
+  def findKth[A](k: Int, l: List[A]): A = {
     try l(k)
     catch {
       case e: IndexOutOfBoundsException => throw new NoSuchElementException
     }
+  }
+
+  def findKthRecursion[A](k: Int, l: List[A]): A = k match {
+    case 0 => l.head // exit case.
+    case k if k > 0 => findKthRecursion(k - 1, l.tail) // standard case.
+
+    case _ => throw new NoSuchElementException // else (covers the empty list case).
   }
 }
