146cc42626bacafcacaeda3b3c65c6f2d8874eb0
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2018-01-29 00:50:58 +0900

    Change concat style of base

--- src/main/scala/P07.scala
+++ src/main/scala/P07.scala
@@ -7,6 +7,6 @@ object P07 {
   def flatten[A](l: List[A]): List[A] = l match {
     case Nil => Nil
     case (h: List[A]) :: tail => flatten(h) ::: flatten(tail) // l の head が A 型の List なのか? (recursive case. どこかのタイミングで最小単位要素となる)
-    case (h: A) :: tail => List(h) ::: flatten(tail)          // head が A 型の最小単位の要素なのか? (base case)
+    case (h: A) :: tail => h :: flatten(tail)                 // head が A 型の最小単位の要素なのか? (base case)
   }
 }
