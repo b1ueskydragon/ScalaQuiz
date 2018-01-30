182e5ed9e7a55917bcebfac2d4ed526fd70ad064
jang <jang_inhwa@microad.co.jp>
2018-01-30 18:08:21 +0900

    P08 WIP

---
+++ src/main/scala/P08.scala
@@ -0,0 +1,10 @@
+object P08 {
+  def main(args :Array[String]): Unit = {
+    println(compress(Dummy.duplicatedList))
+  }
+
+  // Eliminate consecutive duplicates of list elements. The order of the elements should not be changed.
+  def compress[A](l: List[A]): List[A] = l match {
+
+  }
+}
