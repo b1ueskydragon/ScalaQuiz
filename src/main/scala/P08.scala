dff3559fefc3ec4ebf11f3f739f6f3a62f61a690
jang <jang_inhwa@microad.co.jp>
2018-01-31 16:46:36 +0900

    P08: Have some questions

--- src/main/scala/P08.scala
+++ src/main/scala/P08.scala
@@ -4,20 +4,19 @@
   */
 object P08 {
   def main(args: Array[String]): Unit = {
-    val testList = Dummy.duplicatedList
-    // println(compressRecursion(Dummy.duplicatedList))
-    println(compressRecursionHelper(testList, testList.head))
+    println(compressRecursion(Dummy.duplicatedList))
   }
 
+  // TODO ただの重複除去でよかった??
   def compressRecursion[A](l: List[A]): List[A] = l match {
     case Nil => Nil
-    case lst => compressRecursionHelper(lst, lst.head)
- //   case
+    case h :: List() => List(h)
+    case h :: tail if h == tail.head => compressRecursion(tail)
+    case h :: tail => h :: compressRecursion(tail)
   }
 
   def compressRecursionHelper[A](l: List[A], h: A): List[A] = {
     h :: l.tail.filter(_ != h) // h は List の　head
-    // l.head :: l.tail.filter(_ != l.head)
   }
 
   // TODO buffer を間に挟む方法. 試す.
