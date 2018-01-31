73920e03880cd6f5c6c9f3c0b90671150bd5d6f9
jang <jang_inhwa@microad.co.jp>
2018-01-31 13:55:57 +0900

    P08: WIP

--- src/main/scala/P08.scala
+++ src/main/scala/P08.scala
@@ -4,11 +4,21 @@
   */
 object P08 {
   def main(args: Array[String]): Unit = {
-  //  println(compressRecursion(Dummy.duplicatedList))
-    println(compressRecursionHelper(Dummy.duplicatedList))
+    val testList = Dummy.duplicatedList
+    // println(compressRecursion(Dummy.duplicatedList))
+    println(compressRecursionHelper(testList, testList.head))
   }
 
-  def compressRecursionHelper[A](l: List[A]): List[A] = l match {
-    case h :: tail => h :: tail.filter(_ != h)
+  def compressRecursion[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case lst => compressRecursionHelper(lst, lst.head)
+ //   case
   }
+
+  def compressRecursionHelper[A](l: List[A], h: A): List[A] = {
+    h :: l.tail.filter(_ != h) // h は List の　head
+    // l.head :: l.tail.filter(_ != l.head)
+  }
+
+  // TODO buffer を間に挟む方法. 試す.
 }
