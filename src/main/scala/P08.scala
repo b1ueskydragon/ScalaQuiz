4f46a261b37d820a50ca1fc18005b84ef7091e99
jang <jang_inhwa@microad.co.jp>
2018-01-31 13:38:59 +0900

    P08: Make Helper

--- src/main/scala/P08.scala
+++ src/main/scala/P08.scala
@@ -3,14 +3,12 @@
   * The order of the elements should not be changed.
   */
 object P08 {
-  def prototype01[A](l: List[A]): List[A] = l match {
-    case Nil => Nil
-    case h :: tail => h :: prototype01(tail)
-    case List(e0, e1) if e0 == e1 => List(e0)
+  def main(args: Array[String]): Unit = {
+  //  println(compressRecursion(Dummy.duplicatedList))
+    println(compressRecursionHelper(Dummy.duplicatedList))
   }
 
-  def prototype02[A](l: List[A]): List[A] = l match {
-    case Nil => Nil
-    case h::tail if h == tail.head => h :: tail.tail
+  def compressRecursionHelper[A](l: List[A]): List[A] = l match {
+    case h :: tail => h :: tail.filter(_ != h)
   }
 }
