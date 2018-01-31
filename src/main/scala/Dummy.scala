4efcb4603785c919cbe2ae3f795ee922a588bc78
jang <jang_inhwa@microad.co.jp>
2018-01-31 13:38:22 +0900

    Prototypes P08

--- src/main/scala/Dummy.scala
+++ src/main/scala/Dummy.scala
@@ -3,5 +3,5 @@ object Dummy {
   val yourList = List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
   val aList = List(8)
   val nestedList = List(List(1, 2), 3, List(4, List(5, 6)))
-  val duplicatedList = List('a','a','b','b','a','a','c','a','c,'c','a','d','a');
+  val duplicatedList = List('a', 'a', 'b', 'b', 'a', 'a', 'c', 'a', 'c', 'c', 'a', 'd', 'a')
 }
--- src/main/scala/P08.scala
+++ src/main/scala/P08.scala
@@ -1,10 +1,16 @@
+/**
+  * Eliminate consecutive duplicates of list elements.
+  * The order of the elements should not be changed.
+  */
 object P08 {
-  def main(args :Array[String]): Unit = {
-    println(compress(Dummy.duplicatedList))
+  def prototype01[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case h :: tail => h :: prototype01(tail)
+    case List(e0, e1) if e0 == e1 => List(e0)
   }
 
-  // Eliminate consecutive duplicates of list elements. The order of the elements should not be changed.
-  def compress[A](l: List[A]): List[A] = l match {
-
+  def prototype02[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case h::tail if h == tail.head => h :: tail.tail
   }
 }
