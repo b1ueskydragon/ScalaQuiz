849854ac6478b7913b2c6864c4f2f3cd57fc0ee2
jang <jang_inhwa@microad.co.jp>
2018-02-02 19:20:46 +0900

    Add comment

--- src/main/scala/P08.scala
+++ src/main/scala/P08.scala
@@ -7,17 +7,13 @@ object P08 {
     println(compressRecursion(Dummy.duplicatedList))
   }
 
-  // TODO ただの重複除去でよかった??
   def compressRecursion[A](l: List[A]): List[A] = l match {
     case Nil => Nil
-    case h :: List() => List(h)
-    case h :: tail if h == tail.head => compressRecursion(tail)
-    case h :: tail => h :: compressRecursion(tail)
+    case h :: List() => List(h)                                 // exit case. 一つしかない場合そのまま返す
+    case h :: tail if h == tail.head => compressRecursion(tail) // standard case 1. head と tail.head が同じなら tail.head を持ってきて 元の head をとる
+    case h :: tail => h :: compressRecursion(tail)              // standard case 2. head と tail.head が異なる場合, 何もしない (そのままくっつける)
   }
 
-  def compressRecursionHelper[A](l: List[A], h: A): List[A] = {
-    h :: l.tail.filter(_ != h) // h は List の　head
-  }
-
+  //def compressRecursionHelper[A](l: List[A]): List[A] = l.head :: l.tail.filter(_ != l.head) // h は List の　head
   // TODO buffer を間に挟む方法. 試す.
 }
