4bd28569de1bc09e07b7eb32d831b1cd800c239b
inagon <dragoalie@gmail.com>
2018-03-18 22:22:18 +0900

    WIPWIPWIP

--- src/main/scala/P09.scala
+++ src/main/scala/P09.scala
@@ -2,12 +2,30 @@
   * Pack consecutive duplicates of list elements into sublists.
   */
 object P09 {
-  def main(args: Array[String]): Unit = {
-    println(pack(Dummy.duplicatedList2))
+  def main(args: Array[String]) = {
+    lazy val duplicatedList = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+
+    println(pack(List('a)))
+    println(_pack(List(), duplicatedList))
   }
 
-  def pack[A](l: List[A]): List[A] = l match {
+  def pack[A](l: List[A]): List[List[A]] = l match {
     case Nil => Nil
-    //case
+    case List(a) => List(List(a)) // base case
+    case List(a, b) => List(List(a), List(b))
+  }
+
+  /**
+    *
+    * @param res a result (List of lists)
+    * @param rem an element of List of lists
+    * @tparam A  any type
+    * @return    res (recursion)
+    */
+  def _pack[A](res: List[List[A]], rem: List[A]): List[List[A]] = rem match {
+    case Nil => res
+    case h::tail
+      if res.isEmpty || res.last.head != h => _pack(res:::List(List(h)), tail)
+    case h::tail => _pack(res.init:::List(res.last:::List(h)), tail)
   }
 }
