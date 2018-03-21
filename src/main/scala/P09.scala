cff5968033fe2b51c9baa90fc55191e49ac845ce
inagon <dragoalie@gmail.com>
2018-03-21 10:41:16 +0900

    Requirements sufficiency ver

--- src/main/scala/P09.scala
+++ src/main/scala/P09.scala
@@ -1,50 +1,54 @@
 /**
   * Pack consecutive duplicates of list elements into sublists.
+  *
+  * 重複を除いている訳ではない
   */
 object P09 {
   def main(args: Array[String]) = {
-    lazy val duplicatedList = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
-
-    //println(pack(List('a)))
-    //println(_pack(List(), duplicatedList))
-
-    println(packPack(List(), List('a)))
+    lazy val duplicatedList = List('a, 'a, 'a, 'b, 'b, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    println(pack(List(), duplicatedList))
   }
 
   // initial approach.
-  def pack[A](l: List[A]): List[List[A]] = l match {
+  def __pack[A](l: List[A]): List[List[A]] = l match {
     case Nil => Nil
     case List(a) => List(List(a)) // base case
     case List(a, b) => List(List(a), List(b))
   }
 
   /**
-    * 完成の状態 (res) を先に想像して逆算する (はず)
+    * 重複を許すパターン (破壊的?)
     *
-    * @param res a result (List of lists)
-    * @param rem an element of List of lists
-    * @tparam A  any type
-    * @return    res (recursion)
+    * @param outer    bigger one (initial status is Nil)
+    * @param unpacked a target list unpacked yet (elements of outer)
+    * @tparam A any type
+    * @return outer
     */
-  def _pack[A](res: List[List[A]], rem: List[A]): List[List[A]] = rem match {
-    case Nil => res
-    case h::tail
-      if res.isEmpty || res.last.head != h => _pack(res:::List(List(h)), tail)
-    case h::tail => _pack(res.init:::List(res.last:::List(h)), tail)
-  }
-
-
-  /**
-    *
-    * @param outer bigger one (initial status is Nil)
-    * @param inner elements of outer
-    * @tparam A    any type
-    * @return      bigger one
-    */
-  def packPack[A](outer: List[List[A]], inner: List[A]): List[List[A]] = inner match {
+  def pack[A](outer: List[List[A]], unpacked: List[A]): List[List[A]] = unpacked match {
+    /*
+     * exit case
+     *
+     * 着目が全て終わったら(h::tail の これ以上返せる tail がなかったら)結果を返す
+     */
     case Nil => outer
-    case List(a) => outer:::List(List(a))
-//    case h::tail =>
-//      if h != tail.head =>
+
+    /*
+     * standard if case
+     *
+     * outer がまだ empty の時を含め、
+     * h に対する pack がまだない -> 新しい pack を append する
+     */
+    case h :: tail
+      if outer.isEmpty || outer.last.head != h =>
+      pack(outer ::: List(List(h)), tail)
+
+    /*
+     * standard else case
+     *
+     * h に対する pack がすでに(末尾に)存在する -> 該当 pack に入れる
+     * (init: last を除いたこれまでの要素)
+     */
+    case h :: tail =>
+      pack(outer.init ::: List(outer.last ::: List(h)), tail)
   }
 }
