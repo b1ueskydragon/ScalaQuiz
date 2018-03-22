5cb7ba23d168368bd90961c60d95987905e98546
jang <jang_inhwa@microad.co.jp>
2018-03-22 20:25:07 +0900

    WIP add TODO about immutable

--- src/main/scala/P09Another.scala
+++ src/main/scala/P09Another.scala
@@ -1,6 +1,10 @@
 
 /**
   * P09 の重複を許さないパターン
+  *
+  * for 文と違い collection によるループはリストを破壊せず(新しく作るので)実装できる.
+  * 副作用防止にも繋がる.
+  *
   */
 object P09Another {
   def main(args: Array[String]) = {
@@ -9,6 +13,7 @@ object P09Another {
   }
 
   /**
+    * outer においてとても破壊的
     *
     * @param unpacked a target list unpacked yet (elements of outer)
     * @tparam A any type
@@ -30,8 +35,10 @@ object P09Another {
         table += (i -> lastIdx)
       }
       else {
-        val toPutIdx = table.getOrElse(i, 0)
-        println(i, toPutIdx)
+        val toPutIdx: Int = table.getOrElse(i, 0)
+
+        outer.patch(toPutIdx, Seq(outer(toPutIdx) ::: List(i)), 1) // immutable...
+        // TODO Immutable List + collection で実装する.
       }
     }
 
