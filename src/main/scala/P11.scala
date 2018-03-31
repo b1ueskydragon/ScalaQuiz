83f59eac0d7782fea9b61d1ab0e17211287ea4d7
inagon <dragoalie@gmail.com>
2018-04-01 01:17:18 +0900

    P11 Done

--- src/main/scala/P11.scala
+++ src/main/scala/P11.scala
@@ -3,25 +3,23 @@ import P10._
 object P11 {
   def main(args: Array[String]): Unit = {
     val target = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
-
-    // List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e))
-
-    println(_encodeModified(List(), encode(target)))
+    println(encodeModified(encode(target)))
   }
 
   // Modify the result of problem P10
   // param `l` should be target
-  def encodeModified[A](l: List[A]) = {
-  }
+  def encodeModified[A](l: List[(Int, A)]) = {
+    // param `tupList` should be result of P10#encode
+    // return type is List[Any] ( != List[A])
+    def _encodeModified(result: List[Any], tupList: List[(Int, A)]): List[Any] = tupList match {
+      case Nil => result
+      case h :: tail
+        if h._1 == 1 =>
+        _encodeModified(result ::: List(h._2), tail)
+      case h :: tail =>
+        _encodeModified(result ::: List(h), tail)
+    }
 
-  // param `tupList` should be result of P10#encode
-  // return type is List[Any] ( != List[A])
-  def _encodeModified[A](result: List[Any], tupList: List[(Int, A)]): List[Any] = tupList match {
-    case Nil => result
-    case h :: tail
-      if h._1 == 1 =>
-      _encodeModified(result ::: List(h._2), tail)
-    case h :: tail =>
-      _encodeModified(result ::: List(h), tail)
+    _encodeModified(List(), l)
   }
 }
