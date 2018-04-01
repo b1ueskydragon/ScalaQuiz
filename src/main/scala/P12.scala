51ca1e0219aa810a792b6e2469d1963c2fa05ff1
inagon <dragoalie@gmail.com>
2018-04-02 00:08:20 +0900

    P12 done

--- src/main/scala/P12.scala
+++ src/main/scala/P12.scala
@@ -1,14 +1,18 @@
 object P12 {
   def main(args: Array[String]): Unit = {
     val target = List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))
-    println(_decode(List(), target))
+    println(decode(target))
   }
 
-  def _decode[A](result: List[A], encoded: List[(Int, A)]): List[A] = encoded match {
-    case Nil => result
-    case h :: tail
-      if h._1 > 1 =>
-      _decode(result ::: List(h._2), (h._1 - 1, h._2) :: tail)
-    case h :: tail => _decode(result ::: List(h._2), tail)
+  def decode[A](encoded: List[(Int, A)]): List[A] = {
+    def _decode(result: List[A], encoded: List[(Int, A)]): List[A] = encoded match {
+      case Nil => result
+      case h :: tail
+        if h._1 > 1 =>
+        _decode(result ::: List(h._2), (h._1 - 1, h._2) :: tail)
+      case h :: tail => _decode(result ::: List(h._2), tail)
+    }
+
+    _decode(List(), encoded)
   }
 }
