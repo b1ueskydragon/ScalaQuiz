849c776e5ca235d7cfc41711f2d824b75228c69a
inagon <dragoalie@gmail.com>
2018-04-02 08:19:30 +0900

    Add straightfoward one

--- src/main/scala/P11.scala
+++ src/main/scala/P11.scala
@@ -4,11 +4,12 @@ object P11 {
   def main(args: Array[String]): Unit = {
     val target = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
     println(encodeModified(encode(target)))
+    println(encodeModifiedStraight(target))
   }
 
   // Modify the result of problem P10
   // param `l` should be target
-  def encodeModified[A](l: List[(Int, A)]) = {
+  def encodeModified[A](l: List[(Int, A)]): List[Any] = {
     // param `tupList` should be result of P10#encode
     // return type is List[Any] ( != List[A])
     def _encodeModified(result: List[Any], tupList: List[(Int, A)]): List[Any] = tupList match {
@@ -21,5 +22,10 @@ object P11 {
     }
 
     _encodeModified(List(), l)
+  }
+
+  def encodeModifiedStraight[A](l: List[A]): List[Any] = encode(l) map {
+    case e if e._1 == 1 => e._2
+    case e => e
   }
 }
