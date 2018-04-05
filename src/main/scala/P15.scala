d6a18f8787d0ed90a1fafc08ea9424f807e7a8f6
jang <jang_inhwa@microad.co.jp>
2018-04-05 19:23:52 +0900

    Simplify more

--- src/main/scala/P15.scala
+++ src/main/scala/P15.scala
@@ -2,15 +2,24 @@ object P15 {
   def main(args: Array[String]): Unit = {
     val target = List('a, 'b, 'c, 'c, 'd)
     println(duplicateN(3, target))
+    println()
+    println(duplicateNUseFill(3, target))
+
   }
 
   def duplicateN[A](n: Int, ori: List[A]): List[A] = {
     val globalN = n
+
     def _duplicateN(n: Int, result: List[A], ori: List[A]): List[A] = ori match {
       case Nil => result
       case lst if n > 1 => _duplicateN(n - 1, result ::: List(lst.head), ori)
       case h :: tail => _duplicateN(globalN, result ::: List(h), tail)
     }
+
     _duplicateN(globalN, List(), ori)
+  }
+
+  def duplicateNUseFill[A](n: Int, ori: List[A]): List[A] = ori.flatMap {
+    e => List.fill(n)(e)
   }
 }
