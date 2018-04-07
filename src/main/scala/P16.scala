ce4365c7a275b538d611f04e0bd6187d5cfa6c55
inagon <dragoalie@gmail.com>
2018-04-07 13:53:18 +0900

    Simplify

--- src/main/scala/P16.scala
+++ src/main/scala/P16.scala
@@ -1,23 +1,12 @@
 object P16 {
   def main(args: Array[String]): Unit = {
-    val target = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'zz, 'z2)
-    println(_drop(List(), 3, target))
-    // List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k)
+    val target = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+    println(drop(3, target))
   }
 
-  def _drop[A](result: List[A], n: Int, l: List[A]): List[A] = l.flatMap {
-    case e if l.indexOf(e) % n == n - 1 => result ::: List()
-    case e => result ::: List(e)
-  }
-
-  // よくない例
-  def packByN[A](res: List[A], tmp: List[A], n: Int, l: List[A]): List[Any] = l.map {
-    case e if tmp.length < n =>
-      println("1" + e)
-      tmp ::: List(e) // なぜならここで毎回新しい tmp 作成される
-    case e if tmp.length == n =>
-      println("2" + e)
-      res ::: tmp
+  def drop[A](n: Int, l: List[A]): List[A] = l.flatMap {
+    case e if l.indexOf(e) % n == n - 1 => List() ::: List() // 毎回新しく生成される
+    case e => List() ::: List(e) // same as above
   }
 }
 
