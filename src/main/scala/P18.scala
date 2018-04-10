1e932e3eb688fce9f1e5dbc6e5605f30b22b58fe
jang <jang_inhwa@microad.co.jp>
2018-04-10 14:59:00 +0900

    done. little bit weird ver

--- src/main/scala/P18.scala
+++ src/main/scala/P18.scala
@@ -1,11 +1,11 @@
 object P18 {
   def main(args: Array[String]): Unit = {
     val target = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
-    println(slice(List(), 3, 7, target)) //List('d, 'e, 'f, 'g)
+    println(slice(3, 7, target)) //List('d, 'e, 'f, 'g)
   }
 
-  def slice[A](result: List[A], to: Int, from: Int, l: List[A]): List[A] = l match {
-    case Nil => result
-    case h :: tail if tail.length > from => slice(result ::: List(h), to, from - 1, tail)
-  }
+  def slice[A](to: Int, from: Int, l: List[A]): List[Any] = l.map {
+    case e if l.indexOf(e) >= to && l.indexOf(e) < from => e
+    case _ => ""
+  }.filter(e => e != "")
 }
