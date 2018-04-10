9d9a0c5774669e6f2b708b383c3525f5157dd1bd
jang <jang_inhwa@microad.co.jp>
2018-04-10 15:36:38 +0900

    Add todo

--- src/main/scala/P18.scala
+++ src/main/scala/P18.scala
@@ -2,9 +2,14 @@ object P18 {
   def main(args: Array[String]): Unit = {
     val target = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
     println(slice(3, 7, target)) //List('d, 'e, 'f, 'g)
+    println(sliceFunction(3, 7, target))
   }
 
   def slice[A](to: Int, from: Int, l: List[A]): List[A] = l.drop(to).dropRight(from - to)
+
+  def sliceFunction[A](i: Int, k: Int, l: List[A]): List[A] = l.slice(i, k)
+
+  // TODO Add recursive function.
 
   // ... if there are duplicated element, it will not work
   def sliceIndexFilter[A](to: Int, from: Int, l: List[A]): List[A] = l.filter(e => l.indexOf(e) >= to && l.indexOf(e) < from)
