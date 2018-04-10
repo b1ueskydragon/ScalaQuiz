597cdc4f9bb92e81b214ef467451c99d5e217144
jang <jang_inhwa@microad.co.jp>
2018-04-10 15:01:37 +0900

    simplify weird ver

--- src/main/scala/P18.scala
+++ src/main/scala/P18.scala
@@ -3,9 +3,6 @@ object P18 {
     val target = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
     println(slice(3, 7, target)) //List('d, 'e, 'f, 'g)
   }
-
-  def slice[A](to: Int, from: Int, l: List[A]): List[Any] = l.map {
-    case e if l.indexOf(e) >= to && l.indexOf(e) < from => e
-    case _ => ""
-  }.filter(e => e != "")
+  // ... if there are duplicated element, it will not work
+  def slice[A](to: Int, from: Int, l: List[A]): List[A] = l.filter(e => l.indexOf(e) >= to && l.indexOf(e) < from)
 }
