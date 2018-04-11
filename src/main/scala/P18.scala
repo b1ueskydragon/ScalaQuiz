eba1d8509b24366aa38424af366a2c542ac3a956
inagon <dragoalie@gmail.com>
2018-04-12 08:51:43 +0900

    Add recursive case

--- src/main/scala/P18.scala
+++ src/main/scala/P18.scala
@@ -1,10 +1,11 @@
 object P18 {
   def main(args: Array[String]): Unit = {
     val target = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
-    println(sliceDrop(3, 7, target)) //List('d, 'e, 'f, 'g)
+    println(sliceDrop(3, 7, target))
     println(sliceFunction(3, 7, target))
     println(sliceTake(3, 7, target))
     println(sliceRecursion(3, 7, target))
+    println(sliceRecursionTail(3, 7, target))
   }
 
   def sliceDrop[A](to: Int, from: Int, l: List[A]): List[A] = l.drop(to).dropRight(from - to)
@@ -13,12 +14,30 @@ object P18 {
 
   def sliceTake[A](to: Int, from: Int, l: List[A]): List[A] = l.take(from).drop(to)
 
-  def sliceRecursion[A](start: Int, end: Int, l: List[A]): List[A] = (start, end, l) match {
-    case (0, 0, rst) => rst
-    case (0, e, rst) => sliceRecursion(0, e - 1, l.init)
-    case (s, e, h :: tail) => sliceRecursion(s - 1, e, tail)
+  // start and end point is fixed.
+  def sliceRecursion[A](start: Int, end: Int, l: List[A]): List[A] = {
+    def _recursion(cursor: Int, current: List[A], rst: List[A]): List[A] = (cursor, current) match {
+      case (_, Nil) => rst // 何もしない
+      case (c, h :: tail) if end <= c => println(h + " : go out of range"); rst // 何もしない
+      case (c, h :: tail) if start <= c => println(h + " : enter in range"); _recursion(c + 1, tail, rst ::: List(h))
+      case (c, h :: tail) => println(h + " : heading to start point"); _recursion(c + 1, tail, rst)
+    }
+
+    _recursion(0, l, Nil)
   }
 
-  // ... if there are duplicated element, it will not work
+  // same as above but tail recursive.
+  def sliceRecursionTail[A](start: Int, end: Int, l: List[A]): List[A] = {
+    def _recursion(cursor: Int, current: List[A], rst: List[A]): List[A] = (cursor, current) match {
+      case (_, Nil) => rst.reverse
+      case (c, h :: tail) if end <= c => rst.reverse
+      case (c, h :: tail) if start <= c => _recursion(c + 1, tail, h :: rst)
+      case (c, h :: tail) => _recursion(c + 1, tail, rst)
+    }
+
+    _recursion(0, l, Nil)
+  }
+
+  // ... if there are duplicated element, it won't work
   def sliceIndexFilter[A](to: Int, from: Int, l: List[A]): List[A] = l.filter(e => l.indexOf(e) >= to && l.indexOf(e) < from)
 }
