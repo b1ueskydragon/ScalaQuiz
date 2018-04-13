f77f28d232fcab95289d8801e405a023eb29d23d
jang <jang_inhwa@microad.co.jp>
2018-04-13 19:10:22 +0900

    Add another case

--- src/main/scala/P16.scala
+++ src/main/scala/P16.scala
@@ -3,6 +3,7 @@ object P16 {
     val target = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
     println(drop(3, target))
     println(dropTailRecursion(3, target))
+    println(dropRecursion(3, target))
   }
 
   def drop[A](n: Int, l: List[A]): List[A] = l.flatMap {
@@ -10,6 +11,7 @@ object P16 {
     case e => List() ::: List(e) // same as above
   }
 
+  // Tail Recursive.
   // 再帰中 drop したい head だけ result にくっつけない.
   def dropTailRecursion[A](n: Int, l: List[A]): List[A] = {
     def _prepend(cnt: Int, current: List[A], rst: List[A]): List[A] = (cnt, current) match {
@@ -19,6 +21,16 @@ object P16 {
     }
 
     _prepend(n, l, List())
+  }
+
+  def dropRecursion[A](n: Int, l: List[A]): List[A] = {
+    def _append(cnt: Int, current: List[A]): List[A] = (cnt, current) match {
+      case (_, Nil) => Nil
+      case (1, _ :: tail) => _append(n, tail)
+      case (_, h :: tail) => h :: _append(cnt - 1, tail)
+    }
+
+    _append(n, l)
   }
 }
 
