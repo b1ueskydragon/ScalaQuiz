d4ff2e50e8754f82f77a22b38abfa2396871b979
inagon <dragoalie@gmail.com>
2018-04-20 07:39:45 +0900

    Make it work first

---
+++ src/main/scala/P20.scala
@@ -0,0 +1,16 @@
+object P20 {
+  def main(args: Array[String]): Unit = {
+    val target = List('a, 'b, 'c, 'd)
+    println(removeAt(3, target)) // (List('a, 'c, 'd),'b)
+  }
+
+  def removeAt[A](n: Int, l: List[A]): (List[A], A) = {
+    def _recursion(sub: List[A], cnt: Int, l: List[A]): List[A] = l match {
+      case Nil => sub
+      case _ if cnt == 0 => _recursion(sub, cnt - 1, l.tail)
+      case _ => _recursion(sub :+ l.head, cnt - 1, l.tail)
+    }
+
+    (_recursion(Nil, n, l), l(n))
+  }
+}
