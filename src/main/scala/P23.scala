ca97caa973019e2086793512df0004880b47cb82
jang <dragoalie@gmail.com>
2018-04-25 00:32:41 +0900

    Make it work first cases

---
+++ src/main/scala/P23.scala
@@ -0,0 +1,27 @@
+import scala.util.Random
+
+object P23 {
+  def main(args: Array[String]): Unit = {
+    val target = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h)
+    println(randomSelectRec(3, target))
+    println(randomSelectTake(3, target))
+    println(randomSelectDrop(3, target))
+
+    // e.g. List('e, 'd, 'a)
+  }
+
+  def randomSelectRec[A](n: Int, l: List[A]): List[A] = {
+    def _recursion(cnt: Int, l: List[A], rst: List[A]): List[A] = Random.shuffle(l) match {
+      case h :: tail if cnt < n => _recursion(cnt + 1, tail, rst :+ h)
+      case _ if n == cnt => rst
+    }
+
+    _recursion(0, l, Nil)
+  }
+
+  def randomSelectTake[A](n: Int, l: List[A]): List[A] = Random.shuffle(l).take(n)
+
+  def randomSelectDrop[A](n: Int, l: List[A]): List[A] = Random.shuffle(l).drop(l.length - n)
+
+  // TODO Use the solution to problem P20
+}
