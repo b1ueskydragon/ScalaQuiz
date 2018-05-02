730d59f1ccda2660bed8c85f3e0cb82c29de57c1
jang <jang_inhwa@microad.co.jp>
2018-05-02 14:04:43 +0900

    Add cmts abt each cases

--- src/main/scala/P26.scala
+++ src/main/scala/P26.scala
@@ -2,26 +2,25 @@ import scala.collection.mutable
 
 object P26 {
   def main(args: Array[String]): Unit = {
-    val sample = List('a, 'b, 'c)
-    println(combinations(2, sample))
+    val sample = List('a, 'b, 'c, 'd, 'e, 'f)
+    println(combinations(3, sample))
 
     // generate all the possibilities C(N,K)
     // List(List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), ...
   }
 
   // all of each elements (type List[A]) length is same as `n`.
+  // eachList is looks like a stack.
 
   def combinations[A](n: Int, origin: List[A]): List[List[A]] = {
     val outer = new mutable.MutableList[List[A]]
 
-    def recursion(current: List[A], eachList: List[A]): mutable.MutableList[List[A]] =
-      current match {
-
-        case _ if eachList.size == n => outer += eachList
-        case h :: tail if current.size + eachList.size >= n =>
-          recursion(tail, h :: eachList)
-          recursion(tail, eachList)
-        case _ => outer
+    def recursion(current: List[A], eachList: List[A]): mutable.MutableList[List[A]] = current match {
+        case _ if eachList.length == n => outer += eachList.reverse // concat to Result
+        case _ if current.length + eachList.length < n => outer // nothing to do, since current and eachList cannot generate `n` length parts.
+        case h :: tail =>
+          recursion(tail, h :: eachList) // recursion①
+          recursion(tail, eachList) // recursion②, each after exit from recursion①
       }
 
     recursion(origin, List()).toList
