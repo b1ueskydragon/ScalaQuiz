75f336f03d1fa56e69e75cea738d17ef39204c13
jang <jang_inhwa@microad.co.jp>
2018-05-02 11:43:44 +0900

    Use length of each parts

--- src/main/scala/P26.scala
+++ src/main/scala/P26.scala
@@ -1,13 +1,29 @@
+import scala.collection.mutable
+
 object P26 {
   def main(args: Array[String]): Unit = {
-    val sample = List('a, 'b, 'c, 'd, 'e, 'f)
-    println(combinations(3, sample))
+    val sample = List('a, 'b, 'c)
+    println(combinations(2, sample))
 
     // generate all the possibilities C(N,K)
     // List(List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), ...
   }
 
-  def combinations[A](n: Int, origin: List[A]): List[List[A]] = {
+  // all of each elements (type List[A]) length is same as `n`.
 
+  def combinations[A](n: Int, origin: List[A]): List[List[A]] = {
+    val outer = new mutable.MutableList[List[A]]
+
+    def recursion(current: List[A], eachList: List[A]): mutable.MutableList[List[A]] =
+      current match {
+
+        case _ if eachList.size == n => outer += eachList
+        case h :: tail if current.size + eachList.size >= n =>
+          recursion(tail, h :: eachList)
+          recursion(tail, eachList)
+        case _ => outer
+      }
+
+    recursion(origin, List()).toList
   }
 }
