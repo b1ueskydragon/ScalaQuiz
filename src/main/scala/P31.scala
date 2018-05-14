b8984bca3c3383cc01607e99a6068f3abd41108a
jang <dragoalie@gmail.com>
2018-05-14 09:35:01 +0900

    Eliminate waste

---
+++ src/main/scala/P31.scala
@@ -0,0 +1,14 @@
+object P31 {
+  def main(args: Array[String]): Unit = {
+    println(isPrime(173))
+    println(isPrime(1681))
+  }
+
+  // Reduce the amount of computation as much as possible.
+  // Is there any other way ?
+  def isPrime(num: Int): Boolean = num match {
+    case _ if num < 2 || num % 2 == 0 => false
+    case _ if num == 2 => true
+    case _ => ((3 to Math.sqrt(num).toInt) by 2) forall (num % _ != 0)
+  }
+}
