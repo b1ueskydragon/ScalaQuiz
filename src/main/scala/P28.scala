a4c871f806c48f8686f7e8768068c2062c0e2fd6
jang <jang_inhwa@microad.co.jp>
2018-05-24 19:00:08 +0900

    Make it work first

---
+++ src/main/scala/P28.scala
@@ -0,0 +1,10 @@
+object P28 {
+  def main(args: Array[String]): Unit = {
+    val given = List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))
+    println(lsort(given))
+    // List[List[Symbol]] = List(List('o), List('d, 'e), List('d, 'e), List('m, 'n), List('a, 'b, 'c), List('f, 'g, 'h), List('i, 'j, 'k, 'l))
+  }
+
+  // sort the elements of the list according to their length.
+  def lsort[A](listsList: List[List[A]]) = listsList.map(l => (l, l.length)).sortWith(_._2 < _._2).map(_._1)
+}
