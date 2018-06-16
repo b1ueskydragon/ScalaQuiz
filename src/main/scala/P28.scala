9b8b75b3236f61a88a7c99aee9d600ac4f6ef4e6
jang <jang_inhwa@microad.co.jp>
2018-06-16 22:56:42 +0900

    WIP

--- src/main/scala/P28.scala
+++ src/main/scala/P28.scala
@@ -1,20 +1,37 @@
 object P28 {
   def main(args: Array[String]): Unit = {
     val given = List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))
-    println(lsort(given)); println(lsortBy(given))
+    println(lsort(given))
+    println(lsortBy(given))
     // List[List[Symbol]] = List(List('o), List('d, 'e), List('d, 'e), List('m, 'n), List('a, 'b, 'c), List('f, 'g, 'h), List('i, 'j, 'k, 'l))
 
     println(lsortFreq(given))
     // List[List[Symbol]] = List(List('i, 'j, 'k, 'l), List('o), List('a, 'b, 'c), List('f, 'g, 'h), List('d, 'e), List('d, 'e), List('m, 'n))
+
+    //    val test = given.map(_.length)
+    //    println(test)
+    //    println(test.map(frequency(_, test)).sortWith(_ < _))
   }
 
   // a) sort the elements of the list according to their length.
   def lsort[A](lists: List[List[A]]): List[List[A]] = lists.map(l => (l, l.length)).sortWith(_._2 < _._2).map(_._1)
+
   def lsortBy[A](lists: List[List[A]]): List[List[A]] = lists.sortBy(_.length)
 
   // b) sort the elements according to their length frequency (a more frequent length come later).
   def lsortFreq[A](lists: List[List[A]]) = {
-    lists.map(_.length)
+    val lengths = lists.map(_.length)
+    lists.map(l => (l, l.length)).map(e => (e._1, frequency(e._2, lengths))).sortWith(_._2 < _._2).map(_._1)
+  }
+
+  def frequency[A](a: A, list: List[A]): Int = {
+    def _rec(stack: Int, l: List[A]): Int = l match {
+      case Nil => stack
+      case h :: tail if h == a => _rec(stack + 1, tail)
+      case _ => _rec(stack, l.tail)
+    }
+
+    _rec(0, list)
   }
 
 }
