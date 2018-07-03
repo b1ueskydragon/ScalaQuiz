e6ae4a789a40174d2d7cd903f9677ef983344f0e
jang <jang_inhwa@microad.co.jp>
2018-07-04 08:26:49 +0900

    Simplify

--- src/main/scala/P32.scala
+++ src/main/scala/P32.scala
@@ -1,11 +1,16 @@
 object P32 {
   //  Use Euclid's algorithm. (長方形を正方形で埋め尽くす)
   def main(args: Array[String]) = {
-    println(gcd(36, 63))
+    val a = 36
+    val b = 63
+    println(gcd(a, b))
+    println(gcd01(a, b))
   }
 
-  def gcd(d: Int, m: Int): Int = m match {
-    case n if n == 0 => d
-    case _ => gcd(m, d % m)
+  def gcd(a: Int, b: Int): Int = b match {
+    case n if n == 0 => a
+    case _ => gcd(b, a % b)
   }
+
+  def gcd01(a: Int, b: Int): Int = if (b == 0) a else gcd01(b, a % b)
 }
