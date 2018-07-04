ba608dc8647941d65ac2542c734cc4cad489a2fa
jang <jang_inhwa@microad.co.jp>
2018-07-04 15:30:10 +0900

    Use class like ..

--- src/main/scala/arithmetic/S99Int.scala
+++ src/main/scala/arithmetic/S99Int.scala
@@ -1,6 +1,12 @@
 package arithmetic {
   class S99Int(val start: Int) {
     import S99Int._
+
+    def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+    gcd(36, 63)
+
+    def isCoprimeTo(n: Int): Boolean = gcd(start, n) == 1
+    35.isCoprimeTo(64)
   }
 
   object S99Int {
