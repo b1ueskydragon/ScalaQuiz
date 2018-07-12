59481627d7ce6a9dbd6b58ed3d35ec1c1ba2ec5b
jang <jang_inhwa@microad.co.jp>
2018-07-13 07:29:47 +0900

    WIP: Use SC, change for-y to filter

--- src/main/scala/P35.scala
+++ src/main/scala/P35.scala
@@ -1,19 +1,17 @@
+import P31._
+
 object P35 {
   //  Construct a flat list containing the prime factors in ascending order.
   def main(args: Array[String]): Unit = {
-    val given = 3156
-    println(primeFactors(given)) // List(3, 3, 5, 7)
+    val given = new java.util.Scanner(System.in).nextInt() // 315
+    println(primeFactors_(given)) // List(3, 3, 5, 7)
   }
 
-  def primeFactors(n: Int): List[Int] = {
-    import P31._
+  def primeFactors(n: Int) = {
+  }
+
+  def primeFactors_(n: Int): List[Int] = {
     val primesToN = (2 to n).filter(isPrime).toList
-
-    for {
-      p <- primesToN
-      if n % p == 0
-    } yield p
-
-    // TODO greedy, dp, ...
+    primesToN.filter(n % _ == 0)
   }
 }
