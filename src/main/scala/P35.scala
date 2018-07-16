ca17f3a9b50ff2df41efc4d6c750aa5a449619e3
jang <jang_inhwa@microad.co.jp>
2018-07-17 00:20:02 +0900

    Refactor (case match to if else)

--- src/main/scala/P35.scala
+++ src/main/scala/P35.scala
@@ -12,10 +12,10 @@ object P35 {
   def primeFactors_(n: Int): List[Int] = {
     val primes = (2 to n).filter(isPrime).toList
 
-    def _rec(n: Int, div: List[Int]): List[Int] = div match {
-      case _ if isPrime(n) => List(n) // exit case
-      case h :: tail if n % h == 0 => h :: _rec(n / h, tail) // s1) 除数で割り切れる場合
-      case _ => _rec(n, primes.tail) // s2) 除数で割り切れない場合
+    def _rec(n: Int, div: List[Int]): List[Int] = {
+      if (isPrime(n)) List(n) // exit case
+      else if (n % div.head == 0) div.head :: _rec(n / div.head, div.tail) // s1) 除数で割り切れる場合
+      else _rec(n, primes.tail) // s2) 除数で割り切れない場合
     }
 
     _rec(n, primes).sortWith(_ < _)
