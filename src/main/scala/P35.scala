1ea43407872d4825fd7e7aebf90eb0923d8caaf5
jang <jang_inhwa@microad.co.jp>
2018-07-17 00:29:10 +0900

    Done: If divisible, restart to origin primes

--- src/main/scala/P35.scala
+++ src/main/scala/P35.scala
@@ -4,20 +4,20 @@ object P35 {
   //  Construct a flat list containing the prime factors in ascending order.
   def main(args: Array[String]): Unit = {
     val given = new java.util.Scanner(System.in).nextInt() // 315
-    val res00 = primeFactors_(given)
+    val res00 = primeFactors(given)
     println(res00) // List(3, 3, 5, 7)
     println(res00.product)
   }
 
-  def primeFactors_(n: Int): List[Int] = {
+  def primeFactors(n: Int): List[Int] = {
     val primes = (2 to n).filter(isPrime).toList
 
     def _rec(n: Int, div: List[Int]): List[Int] = {
       if (isPrime(n)) List(n) // exit case
-      else if (n % div.head == 0) div.head :: _rec(n / div.head, div.tail) // s1) 除数で割り切れる場合
-      else _rec(n, primes.tail) // s2) 除数で割り切れない場合
+      else if (n % div.head == 0) div.head :: _rec(n / div.head, div) // s1) 除数で割り切れる場合
+      else _rec(n, div.tail) // s2) 除数で割り切れない場合
     }
 
-    _rec(n, primes).sortWith(_ < _)
+    _rec(n, primes)
   }
 }
