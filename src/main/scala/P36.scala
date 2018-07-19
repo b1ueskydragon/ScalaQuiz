95b5a8018da19ada2e9fa762e143f453abd68450
jang <jang_inhwa@microad.co.jp>
2018-07-20 02:44:35 +0900

    Use mutable map + rec edition

--- src/main/scala/P36.scala
+++ src/main/scala/P36.scala
@@ -1,4 +1,7 @@
+import P31.isPrime
 import P35._
+
+import scala.collection.mutable
 
 object P36 {
   // Construct a list containing the prime factors and their multiplicity.
@@ -6,10 +9,10 @@ object P36 {
     val given = new java.util.Scanner(System.in).nextInt() // 315
 
     val resList = primeFactorMultiplicityList(given)
-    // val resMap = primeFactorMultiplicityMap(given)
-
     println(resList) // List((3,2), (5,1), (7,1))
-    // println(resMap) // Map(3 -> 2, 5 -> 1, 7 -> 1)
+
+    val resMap = primeFactorMultiplicityMap(given)
+    println(resMap) // Map(3 -> 2, 5 -> 1, 7 -> 1)
   }
 
   def primeFactorMultiplicityList(n: Int): List[(Int, Int)] = {
@@ -22,6 +25,24 @@ object P36 {
     _pack(primeFactors(n), Nil)
   }
 
-  //  def primeFactorMultiplicityMap(n: Int): Map[Int,Int] = {
-  //  }
+  // Do directly
+  def primeFactorMultiplicityMap(n: Int): Map[Int, Int] = {
+    val outer = new mutable.HashMap[Int, Int] // (n -> cnt)
+    val primes = (2 to n).filter(isPrime).toList
+
+    def _rec(n: Int, ps: List[Int], cnt: Int): Unit = {
+      if (isPrime(n)) {
+        if (outer.keySet.contains(n)) outer.update(n, cnt + 1)
+        else outer.update(n, 1)
+      }
+      else if (n % ps.head == 0) {
+        outer.update(ps.head, cnt + 1)
+        _rec(n / ps.head, ps, cnt + 1)
+      }
+      else _rec(n, ps.tail, 0)
+    }
+
+    _rec(n, primes, 0)
+    outer.toMap
+  }
 }
