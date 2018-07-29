9ff01af0c0f1df6634421220f7507739474200d7
jang <jang_inhwa@microad.co.jp>
2018-07-30 00:16:40 +0900

    Add functional + stream case

--- src/main/scala/P39.scala
+++ src/main/scala/P39.scala
@@ -5,10 +5,17 @@ object P39 {
   // construct a list of all prime numbers in that range.
   def main(args: Array[String]): Unit = {
     lazy val given = 7 to 31
-    println(listPrimesinRange(given)) // List(7, 11, 13, 17, 19, 23, 29, 31)
+    // List(7, 11, 13, 17, 19, 23, 29, 31)
+    println(listPrimesinRange(given))
+    println(listPrimesinRange_(given))
   }
 
   def listPrimesinRange(range: Range): List[Int] = {
     range.filter(isPrime_).toList
+  }
+
+  def listPrimesinRange_(range: Range): List[Int] = {
+    val primes = Stream.cons(2, Stream.from(3, 2).filter(isPrime_))
+    primes.dropWhile(_ < range.start).takeWhile(_ <= range.end).toList
   }
 }
