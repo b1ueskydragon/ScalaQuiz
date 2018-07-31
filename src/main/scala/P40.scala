588ef82adbb0f1e78dbcf836f086717246d431bc
jang <jang_inhwa@microad.co.jp>
2018-08-01 00:57:13 +0900

    Done: Initial approach

---
+++ src/main/scala/P40.scala
@@ -0,0 +1,24 @@
+import P31._
+
+object P40 {
+  // Write a function to find the two prime numbers that sum up to a given even integer.
+  def main(args: Array[String]): Unit = {
+    val given = 28
+    lazy val res = goldbach(given)
+    println(res)
+  }
+
+  def goldbach(even: Int): (Int, Int) = {
+    // TODO range setting ?
+    val primes = Stream.from(3, 2).filter(isPrime_).take(even).toList // 2 is excluded
+
+    // rights は最初の状態では left をも含む.
+    def _rec(left: Int, rights: List[Int], origin: List[Int]): (Int, Int) = rights match {
+      case h :: _ if left + h == even => (left, h) // exit case (成立)
+      case h :: tail if left + h < even => _rec(left, tail, origin) // 不成立だけど余地あり
+      case _ => _rec(origin.head, origin, origin.tail) // left そのもののカーソルを右に移動
+    }
+
+    _rec(primes.head, primes, primes)
+  }
+}
