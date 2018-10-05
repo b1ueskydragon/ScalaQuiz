04a739b4d9ab180c2b9183c261554afc3b441867
jang <dragoalie@gmail.com>
2018-10-06 02:12:17 +0900

    Solved? 🤔

--- src/main/scala/P41.scala
+++ src/main/scala/P41.scala
@@ -1,14 +1,14 @@
+import P31._
+
 object P41 {
   // Given a range and print a list of all even numbers and their Goldbach composition.
   def main(args: Array[String]): Unit = {
-    printGoldbachList(9 to 20)
 
-    // Find out how many both primes are bigger than 50 cases in the range 2..3000.
-    //  printGoldbachListLimited(1 to 2000, 50)
-    //  992 = 73 + 919
-    //  1382 = 61 + 1321
-    //  1856 = 67 + 1789
-    //  1928 = 61 + 1867
+    // printGoldbachList(9 to 20)
+
+    // Find out how many both primes are bigger than 50 cases(minimum value) in the given range.
+    printGoldbachListLimited(2 to 3000, 50)
+
   }
 
   def printGoldbachList(range: Range) {
@@ -19,14 +19,18 @@ object P41 {
   }
 
   def printGoldbachListLimited(range: Range, low: Int) {
-    // TODO
+    // 前処理
+    range.filter(k => k % 2 == 0 && k > low * 2).foreach { k =>
+      // 本処理
+      primes.filter(_ > 2).takeWhile(_ < k).find(p => isPrime(k - p)) match {
+        case Some(p) if p > low => println(s"""$k = $p + ${k - p}""")
+        case _ =>
+      }
+    }
   }
 
   def goldbach(even: Int): (Int, Int) = {
-    import P31._
-    val primes = Stream.from(3, 2).filter(isPrime).take(even)
-
-    primes.takeWhile(_ < even).find(p => isPrime(even - p)) match {
+    primes.filter(_ > 2).takeWhile(_ < even).find(p => isPrime(even - p)) match {
       case Some(p) => (p, even - p)
       case _ => throw new IllegalArgumentException
     }
