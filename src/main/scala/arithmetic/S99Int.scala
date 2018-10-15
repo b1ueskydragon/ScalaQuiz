495def57ad9e405bdeb54f53a41878b34c4560bf
jang <dragoalie@gmail.com>
2018-10-16 00:51:13 +0900

    [WIP] Make to full class

--- src/main/scala/arithmetic/S99Int.scala
+++ src/main/scala/arithmetic/S99Int.scala
@@ -3,18 +3,19 @@ package arithmetic {
   class S99Int(val start: Int) {
     import S99Int._
 
-    def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
-    gcd(36, 63)
-
-    def isCoprimeTo(n: Int): Boolean = gcd(start, n) == 1
-    35.isCoprimeTo(64)
-
-    def totient: Int = (1 to start).count(isCoprimeTo)
-    10.totient
+    // P31
+    def isPrime: Boolean =
+      (start > 1) && (primes takeWhile { _ <= Math.sqrt(start) } forall { start % _ != 0 })
   }
 
   object S99Int {
     implicit def int2S99Int(i: Int): S99Int = new S99Int(i)
+
+    val primes = Stream.cons(2, Stream.from(3, 2).filter(_.isPrime)) // Stream is lazy.
+
+    def main(args: Array[String]): Unit = {
+      println(int2S99Int(i=31).isPrime)
+    }
   }
 
 }
