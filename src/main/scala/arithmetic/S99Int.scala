43e6ef86abcd3b1cdb73a48c623807c099376f66
jang <jang_inhwa@microad.co.jp>
2018-10-16 12:49:59 +0900

    Refactor, Add gcd

--- src/main/scala/arithmetic/S99Int.scala
+++ src/main/scala/arithmetic/S99Int.scala
@@ -1,21 +1,32 @@
 package arithmetic {
 
-  class S99Int(val start: Int) {
+  class S99Int(val num: Int) {
+
     import S99Int._
 
-    // P31
-    def isPrime: Boolean =
-      (start > 1) && (primes takeWhile { _ <= Math.sqrt(start) } forall { start % _ != 0 })
+    // Lazy infinite list of primes (Stream is lazy).
+    val primes: Stream[Int] = Stream.cons(hd = 2, tl = Stream.from(3, 2).filter(_.isPrime))
+
+    // Put a limit on `isPrime` to the square root.
+    def isPrime: Boolean = (num > 1) && (primes.takeWhile { ph => ph <= Math.sqrt(num) } forall { ph => num % ph != 0 })
+
+    // Euclid's algorithm.
+    def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
   }
 
   object S99Int {
-    implicit def int2S99Int(i: Int): S99Int = new S99Int(i)
-
-    val primes = Stream.cons(2, Stream.from(3, 2).filter(_.isPrime)) // Stream is lazy.
+    /** @param num Default is 0 */
+    implicit def int2S99Int(num: Int = 0): S99Int = new S99Int(num)
 
     def main(args: Array[String]): Unit = {
-      println(int2S99Int(i=31).isPrime)
+      // P31
+      println(31.isPrime)
+
+      // P32
+      println(int2S99Int().gcd(12, 40))
+
     }
   }
 
 }
+
