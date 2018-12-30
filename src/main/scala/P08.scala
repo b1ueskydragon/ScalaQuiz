49c3f625e1001680a0a630c62c53974ef65b17d2
jang <jang_inhwa@microad.co.jp>
2018-12-31 00:19:28 +0900

    Upgrade to tailrec, Add todo

--- src/main/scala/P08.scala
+++ src/main/scala/P08.scala
@@ -1,12 +1,29 @@
+import scala.annotation.tailrec
+
 /**
   * Eliminate consecutive duplicates of list elements.
   * The order of the elements should not be changed.
   */
 object P08 {
   def main(args: Array[String]): Unit = {
-    val target = List('a', 'a', 'b', 'b', 'a', 'a', 'c', 'a', 'c', 'c', 'a', 'd', 'a')
+    // val target = List('a', 'a', 'b', 'b', 'a', 'a', 'c', 'a', 'c', 'c', 'a', 'd', 'a')
+    val target = "aaabccaadeeee".toList
+    println(compressRec(target))
     println(compressRecursion(target))
   }
+
+  def compressRec[A](l: List[A]): List[A] = {
+    @tailrec
+    def rec(l: List[A], stack: List[A]): List[A] = l match {
+      case Nil => Nil
+      case List(x) => x :: stack
+      case h :: tail => if (h == tail.head) rec(tail, stack) else rec(tail, h :: stack)
+    }
+
+    rec(l, List()).reverse
+  }
+
+  // TODO folding
 
   def compressRecursion[A](l: List[A]): List[A] = l match {
     case Nil => Nil
