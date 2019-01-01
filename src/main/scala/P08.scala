81e6f12bc5983551d08e51b94c8cc8f40856f081
jang <jang_inhwa@microad.co.jp>
2019-01-01 14:14:09 +0900

    Clean code

--- src/main/scala/P08.scala
+++ src/main/scala/P08.scala
@@ -8,11 +8,11 @@ object P08 {
   def main(args: Array[String]): Unit = {
     // val target = List('a', 'a', 'b', 'b', 'a', 'a', 'c', 'a', 'c', 'c', 'a', 'd', 'a')
     val target = "aaabccaadeeee".toList
-    println(compressRec(target))
-    println(compressRecursion(target))
+    println(compress(target))
+    println(compress__(target))
   }
 
-  def compressRec[A](l: List[A]): List[A] = {
+  def compress[A](l: List[A]): List[A] = {
     @tailrec
     def rec(l: List[A], stack: List[A]): List[A] = l match {
       case Nil => Nil
@@ -23,15 +23,11 @@ object P08 {
     rec(l, List()).reverse
   }
 
-  // TODO folding
-
-  def compressRecursion[A](l: List[A]): List[A] = l match {
+  def compress__[A](l: List[A]): List[A] = l match {
     case Nil => Nil
-    case h :: List() => List(h)                                 // exit case. 一つしかない場合そのまま返す
-    case h :: tail if h == tail.head => compressRecursion(tail) // standard case 1. head と tail.head が同じなら tail.head を持ってきて 元の head をとる
-    case h :: tail => h :: compressRecursion(tail)              // standard case 2. head と tail.head が異なる場合, 何もしない (そのままくっつける)
+    case h :: List() => List(h) // exit case. 一つしかない場合そのまま返す
+    case h :: tail if h == tail.head => compress__(tail) // standard case 1. head と tail.head が同じなら tail.head を持ってきて 元の head をとる
+    case h :: tail => h :: compress__(tail) // standard case 2. head と tail.head が異なる場合, 何もしない (そのままくっつける)
   }
 
-  //def compressRecursionHelper[A](l: List[A]): List[A] = l.head :: l.tail.filter(_ != l.head) // h は List の　head
-  // TODO buffer を間に挟む方法. 試す.
 }
