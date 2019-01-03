d410b38046452a02421f20abb1724167f7e88efa
jang <jang_inhwa@microad.co.jp>
2019-01-03 14:04:51 +0900

    Add half as many compares case

--- src/main/scala/P06.scala
+++ src/main/scala/P06.scala
@@ -3,6 +3,8 @@ object P06 {
     val target = List(1, 2, 4, 8, 16, 8, 4, 2, 1)
     println(isPalindrome(target))
     println(isPalindrome_(target))
+    println(isPalindrome__(target))
+    println(isPalindrome___(target))
   }
 
   def isPalindrome[A](l: List[A]): Boolean = l.reverse == l
@@ -12,4 +14,15 @@ object P06 {
     case List(_) => true
     case list => list.head == list.last && isPalindrome_(list.tail.init)
   }
+
+  def isPalindrome__[A](l: List[A]): Boolean = {
+    val half = l.length / 2
+    l.take(half).reverse == l.drop(half + l.length % 2)
+  }
+
+  def isPalindrome___[A](l: List[A]): Boolean = {
+    val t = l.splitAt(l.length / 2)
+    t._1.reverse == t._2.drop(l.length % 2)
+  }
+
 }
