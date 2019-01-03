662cf6b5e7dce564fea6c89a380a0f5798debe82
jang <jang_inhwa@microad.co.jp>
2019-01-03 14:02:41 +0900

    Rename

--- src/main/scala/P06.scala
+++ src/main/scala/P06.scala
@@ -1,15 +1,15 @@
 object P06 {
   def main(args: Array[String]): Unit = {
-    val target = List(1, 2, 3, 2, 1)
+    val target = List(1, 2, 4, 8, 16, 8, 4, 2, 1)
     println(isPalindrome(target))
-    println(isPalindromeRecursion(target))
+    println(isPalindrome_(target))
   }
 
   def isPalindrome[A](l: List[A]): Boolean = l.reverse == l
 
-  def isPalindromeRecursion[A](l: List[A]): Boolean = l match {
+  def isPalindrome_[A](l: List[A]): Boolean = l match {
     case Nil => true
-    case List(a) => true
-    case list => list.head == list.last && isPalindromeRecursion(list.tail.init)
+    case List(_) => true
+    case list => list.head == list.last && isPalindrome_(list.tail.init)
   }
 }
