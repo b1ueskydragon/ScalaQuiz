a2d7616503bad93050f9e1192cdf2b060dd57506
jang <jang_inhwa@microad.co.jp>
2019-01-05 01:27:00 +0900

    Clean code

--- src/main/scala/P04.scala
+++ src/main/scala/P04.scala
@@ -1,23 +1,15 @@
+
 object P04 {
   def main(args: Array[String]): Unit = {
     val target = List(1, 1, 2, 3, 5, 8)
-
     println(length(target))
-    println(lengthRecursive(target))
+    println(length_(target))
   }
 
   def length[A](l: List[A]): Int = l.length
 
-  // List is immutable (a new list is created every time you do a modification)
-  // Array is mutable
-  // https://stackoverflow.com/questions/2712877/difference-between-array-and-list-in-scala
-
-  def lengthRecursive[A](l: List[A]): Int = l match {
-    case Nil => 0 // Nil は空のリスト
-    case h :: tail =>
-      //println(h)
-      1 + lengthRecursive(tail)
-    // :: は既存の List に要素を連結するメソッド、元となる空 List(Nil)が必要
-    // tail は List の戦闘要素を取り除いた List
+  def length_[A](l: List[A]): Int = l match {
+    case Nil => 0
+    case _ :: tail => 1 + length_(tail)
   }
 }
