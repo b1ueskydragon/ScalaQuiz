ed8ad170389f14b75e4c48626d20ede7935811f3
jang <dragoalie@gmail.com>
2019-01-11 08:49:44 +0900

    Clean code

--- src/main/scala/P10.scala
+++ src/main/scala/P10.scala
@@ -1,28 +1,20 @@
+import scala.annotation.tailrec
+
 object P10 {
   def main(args: Array[String]): Unit = {
     val duplicatedList = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
     println(encode(duplicatedList))
   }
 
-  def pack[A](l: List[A]): List[List[A]] = {
-    def _pack(outer: List[List[A]], unpacked: List[A]): List[List[A]] = unpacked match {
-      case Nil => outer
-      case h :: tail // just joining
-        if outer.isEmpty || outer.last.head != h =>
-        _pack(outer ::: List(List(h)), tail)
-      case h :: tail // put into the last pack since same as previous one
-      => _pack(outer.init ::: List(outer.last ::: List(h)), tail)
+  def encode[A](l: List[A]): List[(Int, A)] = {
+    import P09.pack__
+    @tailrec
+    def rec(res: List[(Int, A)], xs: List[List[A]]): List[(Int, A)] = xs match {
+      case Nil => res
+      case h :: tail => rec(res ::: List((h.length, h.head)), tail)
     }
 
-    _pack(List(), l)
+    rec(List(), pack__(l))
   }
 
-  def encode[A](packed: List[A]): List[(Int, A)] = {
-    def _encode(tupList: List[(Int, A)], packed: List[List[A]]): List[(Int, A)] = packed match {
-      case Nil => tupList
-      case h :: tail => _encode(tupList ::: List((h.length, h.head)), tail)
-    }
-
-    _encode(List(), pack(packed))
-  }
 }
