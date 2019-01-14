27f57e243a87e51f9b4643f6798e03f423b289c0
jang <dragoalie@gmail.com>
2019-01-15 00:35:40 +0900

    Clean code

--- src/main/scala/P11.scala
+++ src/main/scala/P11.scala
@@ -1,30 +1,29 @@
 import P10._
 
+import scala.annotation.tailrec
+
+/** Modify the result of problem P10
+  * return type is List[Any]
+  */
 object P11 {
   def main(args: Array[String]): Unit = {
     val target = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
     println(encodeModified(encode(target)))
-    println(encodeModifiedStraight(target))
+    println(encodeModified_(target))
   }
 
-  // Modify the result of problem P10
-  // param `l` should be target
   def encodeModified[A](l: List[(Int, A)]): List[Any] = {
-    // param `tupList` should be result of P10#encode
-    // return type is List[Any] ( != List[A])
-    def _encodeModified(result: List[Any], tupList: List[(Int, A)]): List[Any] = tupList match {
-      case Nil => result
-      case h :: tail
-        if h._1 == 1 =>
-        _encodeModified(result ::: List(h._2), tail)
-      case h :: tail =>
-        _encodeModified(result ::: List(h), tail)
+    @tailrec
+    def rec(res: List[Any], pairs: List[(Int, A)]): List[Any] = pairs match {
+      case Nil => res.reverse
+      case h :: tail if h._1 == 1 => rec(h._2 :: res, tail)
+      case h :: tail => rec(h :: res, tail)
     }
 
-    _encodeModified(List(), l)
+    rec(List(), l)
   }
 
-  def encodeModifiedStraight[A](l: List[A]): List[Any] = encode(l) map {
+  def encodeModified_[A](l: List[A]): List[Any] = encode(l) map {
     case e if e._1 == 1 => e._2
     case e => e
   }
