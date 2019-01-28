182de8470d0f96389cf2f6ed77ffd04a5ab918a0
jang <jang_inhwa@microad.co.jp>
2019-01-28 12:20:12 +0900

    Refactor more, arrived at flatmap

--- src/main/scala/P12.scala
+++ src/main/scala/P12.scala
@@ -4,6 +4,9 @@ object P12 {
   def main(args: Array[String]): Unit = {
     val given = List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))
     println(decode(given))
+    println(decode_(given))
+    println(decode__(given))
+    println(decode___(given))
   }
 
   def decode[A](l: List[(Int, A)]): List[A] = {
@@ -16,4 +19,36 @@ object P12 {
 
     rec(List(), l)
   }
+
+  // refactor above but not simple
+  def decode_[A](l: List[(Int, A)]): List[A] = {
+    // TODO: with lazy eval with stream
+    // TODO: replace to List.fill(r)(e)
+    def replicate(r: Int, e: A): List[A] = {
+      @tailrec
+      def rec(res: List[A], r: Int, e: A): List[A] =
+        if (r == 1) e :: res else rec(e :: res, r - 1, e)
+
+      rec(List(), r, e)
+    }
+
+    @tailrec
+    def rec(res: List[A], xs: List[(Int, A)]): List[A] = xs match {
+      case Nil => res.reverse
+      case h :: tail =>
+        if (h._1 > 1) rec(replicate(h._1, h._2) ::: res, tail)
+        else rec(h._2 :: res, tail)
+    }
+
+    rec(List(), l)
+  }
+
+  // refactor above with foldr. simple but I don't like ::: concatenation
+  def decode__[A](xs: List[(Int, A)]): List[A] = xs.foldRight(List[A]()) { (x, acc) =>
+    if (x._1 > 1) List.fill(x._1)(x._2) ::: acc else x._2 :: acc
+  }
+
+  // do more simply that above with flatten
+  def decode___[A](xs: List[(Int, A)]): List[A] = xs.flatMap(x => List.fill(x._1)(x._2))
+
 }
