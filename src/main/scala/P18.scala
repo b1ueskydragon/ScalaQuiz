aa54c047e1957a548ac02177554e8d7249a6e2b5
jang <jang_inhwa@microad.co.jp>
2019-01-29 13:01:19 +0900

    Foldr umm

--- src/main/scala/P18.scala
+++ src/main/scala/P18.scala
@@ -3,17 +3,21 @@ import scala.annotation.tailrec
 object P18 {
   def main(args: Array[String]): Unit = {
     val xs = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+    //val xs = Nil
     val (i, k) = (3, 7)
+    //val (i, k) = (-2, 400)
+
     println(xs.slice(i, k)) // built-in
     println(slice_(i, k, xs))
-    println(slice__(i, k, xs))
+    //println(slice__(i, k, xs))
     println(slice___(i, k, xs))
     println(slice____(i, k, xs))
+    println(slice_____(i, k, xs))
   }
 
   def slice_[A](from: Int, to: Int, l: List[A]): List[A] = l.take(to).drop(from)
 
-  def slice__[A](from: Int, to: Int, l: List[A]): List[A] = l.drop(from).dropRight(to - from)
+  //def slice__[A](from: Int, to: Int, l: List[A]): List[A] = l.drop(from).dropRight(to - from)
 
   def slice___[A](start: Int, end: Int, l: List[A]): List[A] = {
     @tailrec
@@ -36,6 +40,16 @@ object P18 {
     }
 
     rec(List(), left, right, l)
+  }
+
+  // TODO length is O(N), try foldl
+  def slice_____[A](i: Int, k: Int, xs: List[A]): List[A] = {
+    val xl = xs.length
+    val t = if (k >= xl) xl else k
+    xs.foldRight(xs) { (_, acc) =>
+      val al = acc.length
+      if (al > t) acc.init else if (al > t - i) acc.tail else acc
+    }
   }
 
 }
