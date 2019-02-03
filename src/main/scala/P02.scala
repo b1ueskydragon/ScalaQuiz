0f87b4562ebc48af9063eb13cd1f32cf83125e79
jang <dragoalie@gmail.com>
2019-02-03 12:03:28 +0900

    Separate validate logic, with implicit def

--- src/main/scala/P02.scala
+++ src/main/scala/P02.scala
@@ -1,39 +1,41 @@
 import scala.annotation.tailrec
 
+class P02[A](val xs: List[A]) {
+  def isValid: Boolean = xs.length > 1
+}
+
 object P02 {
+
+  implicit def p02[A](xs: List[A]): P02[A] = new P02[A](xs)
+
   def main(args: Array[String]) {
-    lazy val target = List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
-    println(penultimate(target))
-    println(penultimate_(target))
-    println(penultimate__(target))
-    println(penultimate___(target))
+    val xs = List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+    if (xs.isValid) {
+      println(penultimate(xs))
+      println(penultimate_(xs))
+      println(penultimate__(xs))
+      println(penultimate___(xs))
+    }
   }
 
-  def penultimate[A](l: List[A]): A = {
-    if (l.isEmpty) throw new NoSuchElementException
-    l.init.last
-  }
+  def penultimate[A](xs: List[A]): A = xs.init.last
 
   @tailrec
   def penultimate_[A](l: List[A]): A = l match {
     case List(_, _) => l.head // if only 2 elements (base case).
-    case list => penultimate_(list.tail)
-    case List(_) => throw new NoSuchElementException
+    case xs => penultimate_(xs.tail)
   }
 
-  // same algorithm as above.
   @tailrec
   def penultimate__[A](l: List[A]): A = l match {
     case h :: List(_) => h
     case _ :: tail => penultimate__(tail)
-    case _ => throw new NoSuchElementException
   }
 
   @tailrec
   def penultimate___[A](l: List[A]): A = l match {
     case _ if l.length == 2 => l.head
     case xs => penultimate___(xs.tail)
-    case _ => throw new NoSuchElementException
   }
 
 }
