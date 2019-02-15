add0c162e5047a8601ca7bad07d4cf2af3c12285
jang <dragoalie@gmail.com>
2019-02-16 07:58:50 +0900

    Refactor

--- src/main/scala/P17.scala
+++ src/main/scala/P17.scala
@@ -1,16 +1,24 @@
+import scala.annotation.tailrec
+
 object P17 {
   def main(args: Array[String]): Unit = {
-    val target = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
-    println(split(3, target))
-    println(splitRecursion(3, List(), List(), target))
+    val xs = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+    println(split(3, xs))
+    println(split_(3, xs))
   }
 
-  def split[A](n: Int, l: List[A]): (List[A], List[A]) = l.span(e => l.indexOf(e) < n)
+  def split[A](n: Int, xs: List[A]): (List[A], List[A]) = xs.span(xs.indexOf(_) < n)
 
-  def splitRecursion[A](n: Int, left: List[A], right: List[A], l: List[A]): (List[A], List[A]) = l match {
-    case Nil => (left, right)
-    case h :: tail if left.length < n => splitRecursion(n, left ::: List(h), right, tail)
-    case h :: tail => splitRecursion(n, left, right ::: List(h), tail)
+  def split_[A](n: Int, xs: List[A]): (List[A], List[A]) = {
+    @tailrec
+    def rec(n: Int, left: List[A], right: List[A], l: List[A]): (List[A], List[A]) = l match {
+      case Nil => (left, right)
+      case h :: tail =>
+        if (left.length < n) rec(n, left ::: List(h), right, tail)
+        else rec(n, left, right ::: List(h), tail)
+    }
+
+    rec(n, List(), List(), xs)
   }
 
   // TODO add case
