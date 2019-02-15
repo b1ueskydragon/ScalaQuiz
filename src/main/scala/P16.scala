0ef5210f133883a6a6b1744cb8468756a7ec94af
jang <jang_inhwa@microad.co.jp>
2019-02-15 12:00:32 +0900

    Refactor, Fix wrong logic that used indexof

--- src/main/scala/P16.scala
+++ src/main/scala/P16.scala
@@ -1,53 +1,59 @@
+import scala.annotation.tailrec
+
+/** Drop every Nth element from a list. */
 object P16 {
   def main(args: Array[String]): Unit = {
-    val target = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
-    println(drop(3, target))
-    println(dropTailRecursion(3, target))
-    println(dropRecursion(3, target))
-    println(dropOneLiner(3, target))
-    println(dropAnother(3, target))
-
+    // val n = 0
+    val n = 3
+    val xs = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'c, 'k)
+    println(drop(n, xs))
+    println(drop_(n, xs))
+    println(drop__(n, xs))
+    println(drop___(n, xs))
+    println(drop____(n, xs))
   }
 
-  def dropAnother[A](n: Int, l: List[A]): List[A] = {
-    def _dropOrAppend(res: List[A], cnt: Int, l: List[A]): List[A] = l match {
-      case Nil => res
-      case h :: tail if cnt < (n - 1) => _dropOrAppend(res ::: List(h), cnt + 1, tail)
-      case _ => _dropOrAppend(res, 0, l.tail) // reset counter
+  def drop[A](n: Int, xs: List[A]): List[A] =
+    if (n == 0) xs else xs.grouped(n).flatMap(_.take(n - 1)).toList
+
+  // concat List (:::) is expensive
+  def drop_[A](n: Int, xs: List[A]): List[A] = {
+    @tailrec
+    def rec(cnt: Int, curr: List[A], rst: List[A]): List[A] = (cnt, curr) match {
+      case (_, Nil) => rst
+      case (1, _ :: tail) => rec(n, tail, rst)
+      case (_, h :: tail) => rec(cnt - 1, tail, rst ::: List(h))
     }
 
-    _dropOrAppend(List(), 0, l)
+    rec(n, xs, List())
   }
 
-  def drop[A](n: Int, l: List[A]): List[A] = l.flatMap {
-    case e if l.indexOf(e) % n == n - 1 => List() ::: List() // 毎回新しく生成される
-    case e => List() ::: List(e) // same as above
-  }
-
-  // Tail Recursive.
-  // 再帰中 drop したい head だけ result にくっつけない.
-  def dropTailRecursion[A](n: Int, l: List[A]): List[A] = {
-    def _prepend(cnt: Int, current: List[A], rst: List[A]): List[A] = (cnt, current) match {
-      case (_, Nil) => rst.reverse
-      case (1, _ :: tail) => _prepend(n, tail, rst)
-      case (_, h :: tail) => _prepend(cnt - 1, tail, h :: rst)
-    }
-
-    _prepend(n, l, List())
-  }
-
-  def dropRecursion[A](n: Int, l: List[A]): List[A] = {
-    def _append(cnt: Int, current: List[A]): List[A] = (cnt, current) match {
+  // same logic as above. stack will get deeper if xs has large length.
+  // which one is more efficient?
+  def drop__[A](n: Int, xs: List[A]): List[A] = {
+    def acc(cnt: Int, curr: List[A]): List[A] = (cnt, curr) match {
       case (_, Nil) => Nil
-      case (1, _ :: tail) => _append(n, tail)
-      case (_, h :: tail) => h :: _append(cnt - 1, tail)
+      case (1, _ :: tail) => acc(n, tail)
+      case (_, h :: tail) => h :: acc(cnt - 1, tail)
     }
 
-    _append(n, l)
+    acc(n, xs)
   }
 
-  // make functional.
-  // [[ zipWithIndex ]] python enumerate のような感じで index と value マッピング.
-  def dropOneLiner[A](n: Int, l: List[A]): List[A] = l.zipWithIndex filter (e => (e._2 + 1) % n != 0) map (_._1)
+  // zipWithIndex, mapping index and value
+  def drop___[A](n: Int, xs: List[A]): List[A] =
+    if (n == 0) xs else xs.zipWithIndex.filter(e => (e._2 + 1) % n != 0).map(_._1)
+
+  def drop____[A](n: Int, xs: List[A]): List[A] = {
+    @tailrec
+    def rec(res: List[A], cnt: Int, l: List[A]): List[A] = l match {
+      case Nil => res
+      case h :: tail if cnt < (n - 1) => rec(res ::: List(h), cnt + 1, tail)
+      case _ => rec(res, 0, l.tail) // reset counter
+    }
+
+    rec(List(), 0, xs)
+  }
+
 }
 
