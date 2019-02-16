e6f6742c1e59aabffbe79b5fd2359e8f8fa6d26c
jang <dragoalie@gmail.com>
2019-02-16 18:38:43 +0900

    Add more logics impl splitAt

--- src/main/scala/P17.scala
+++ src/main/scala/P17.scala
@@ -2,9 +2,15 @@ import scala.annotation.tailrec
 
 object P17 {
   def main(args: Array[String]): Unit = {
+    val n = 3
+    //val n = 0
     val xs = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
-    println(split(3, xs))
-    println(split_(3, xs))
+
+    println(xs.splitAt(n))
+    println(split(n, xs))
+    println(split_(n, xs))
+    println(split__(n, xs))
+    println(split___(n, xs))
   }
 
   def split[A](n: Int, xs: List[A]): (List[A], List[A]) = xs.span(xs.indexOf(_) < n)
@@ -21,5 +27,14 @@ object P17 {
     rec(n, List(), List(), xs)
   }
 
-  // TODO add case
+  // simply one
+  def split__[A](n: Int, xs: List[A]): (List[A], List[A]) = (xs.take(n), xs.drop(n))
+
+  def split___[A](n: Int, xs: List[A]): (List[A], List[A]) = {
+    val size = xs.length - n
+    val right = xs.foldRight(List[A]())((x, acc) => if (acc.length < size) x :: acc else acc)
+
+    (xs diff right, right)
+  }
+
 }
