0fc45db5d2e246e0a867a179bd2ab36d61a5d131
jang <dragoalie@gmail.com>
2019-07-20 11:10:13 +0900

    Replace to ListBuffer and append to this

--- src/main/scala/P26.scala
+++ src/main/scala/P26.scala
@@ -1,5 +1,3 @@
-import scala.collection.mutable
-
 /** Generate all the possibilities C(N,K)
   *
   * N := len(xs) | xs is a given list
@@ -8,16 +6,15 @@ import scala.collection.mutable
 object P26 {
 
   def combinations[A](n: Int, xs: List[A]): List[List[A]] = {
-    val res = new mutable.MutableList[List[A]]
+    val res = scala.collection.mutable.ListBuffer.empty[List[A]]
 
     def dfsGen(curr: List[A], ps: List[A]) {
       val k = ps.length
-
       if (curr.isEmpty && k < n) return
       curr match {
-        case _ if k == n => res += ps.reverse
-        case h :: tail =>
-          dfsGen(tail, h :: ps)
+        case _ if k == n => res.append(ps)
+        case h :: tail  =>
+          dfsGen(tail, ps ::: List(h))
           dfsGen(tail, ps)
       }
     }
