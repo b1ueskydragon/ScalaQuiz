7efd638c34529a9e50e19bab58fa7c3ce11ec8d8
ina <dragoalie@gmail.com>
2019-07-21 14:55:18 +0900

    Compare with native combinations

--- src/test/scala/P26Spec.scala
+++ src/test/scala/P26Spec.scala
@@ -5,16 +5,9 @@ class P26Spec extends FunSpec {
 
   describe("combinations") {
 
-    it("should generate sequence of sub-sequences that each length is n") {
+    it("should generate sequence of sub-sequences that each length is k") {
       val (k, xs) = (3, List('a, 'b, 'c, 'd, 'e, 'f))
-
-      val expected =
-        List(List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), List('a, 'b, 'f), List('a, 'c, 'd), List('a, 'c, 'e), List('a, 'c, 'f), List('a, 'd, 'e), List('a, 'd, 'f), List('a, 'e, 'f),
-          List('b, 'c, 'd), List('b, 'c, 'e), List('b, 'c, 'f), List('b, 'd, 'e), List('b, 'd, 'f), List('b, 'e, 'f),
-          List('c, 'd, 'e), List('c, 'd, 'f), List('c, 'e, 'f),
-          List('d, 'e, 'f))
-
-      assert(combinations(k, xs) === expected)
+      assert(combinations(k, xs) === xs.combinations(k).toList)
     }
 
   }
