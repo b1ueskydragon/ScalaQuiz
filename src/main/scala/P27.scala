7c391d90fe435717ab70194fe559300a63cea975
ina <dragoalie@gmail.com>
2019-11-12 12:03:01 +0900

    Replace to built-in combs, rename vals

--- src/main/scala/P27.scala
+++ src/main/scala/P27.scala
@@ -1,31 +1,17 @@
-import P26.combinationsMutable
-
 object P27 {
-  def main(args: Array[String]): Unit = {
-    val sample = List("A", "B", "C", "D", "E", "F", "G", "H", "I")
-    println(group3(sample))
-
-    val pattern = List(2, 3, 4)
-    println(groupN(pattern, sample))
-  }
-
-  // determine first (_,_) (_,_,_) then can get (_,_,_,_) automatically
-  def group3[A](origin: List[A]): List[List[List[A]]] = {
+  // determine first (_,_) (_,_,_) then can get (_,_,_,_) automatically.
+  // (A, B) is the same solution as (B, A), so use combinations.
+  def group3[A](ns: List[A]): List[List[List[A]]] = {
     for {
-      _2 <- combinationsMutable(2, origin)
-      tail = origin diff _2
-      _3 <- combinationsMutable(3, tail)
-    } yield List(_2, _3, tail diff _3)
+      x <- ns.combinations(2).toList
+      notX = ns diff x
+      y <- notX.combinations(3)
+      notXY = notX diff y
+    } yield List(x, y, notXY)
   }
 
-  def groupN[A](pattern: List[Int], origin: List[A]): List[List[List[A]]] = pattern match {
-    case Nil => List(List())
-    case h :: tail =>
-      combinationsMutable(h, origin) flatMap {
-        c =>
-          groupN(tail, origin diff c) map {
-            c :: _
-          }
-      }
+  def groupN[A](pattern: List[Int], ns: List[A]): List[List[List[A]]] = pattern match {
+    case Nil => List(Nil)
+    case k :: ks => ns.combinations(k).toList.flatMap(xs => groupN(ks, ns diff xs) map (xs :: _))
   }
 }
